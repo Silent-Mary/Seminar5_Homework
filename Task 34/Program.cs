619c1a61aaf27f2a07b63cf152e38f55e8ad9c1f
SilentMary <playinglassbead@gmail.com>
2023-09-04 20:54:06 +0100

    initial commit

---
+++ Task 34/Program.cs
@@ -0,0 +1,34 @@
+﻿
+Console.Clear();
+int[] GetRandomArray(int arrayLength1, int start, int end)
+{
+    int[] array = new int[arrayLength1];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next(start, end);
+    }
+    return array;
+}
+
+void PrintArray(int [] arrayToPrint)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        Console.Write(arrayToPrint[i]);
+        if (i < arrayToPrint.Length - 1)
+        {
+            Console.Write (", ");
+        }
+    }
+    Console.Write("] ");
+}
+
+System.Console.WriteLine("Enter array length, min n & max n for range: ");
+int arrayL = Convert.ToInt32(Console.ReadLine());
+int min = Convert.ToInt32(Console.ReadLine());
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] userArray = GetRandomArray(arrayL, min, max);
+PrintArray(userArray);
+System.Console.WriteLine();
---
+++ Task 38/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Clear();
+int[] GetRandomArray(int arrayLength1, int start, int end)
+{
+    int[] array = new int[arrayLength1];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next(start, end);
+    }
+    return array;
+}
+
+void PrintArray(int [] arrayToPrint)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        Console.Write(arrayToPrint[i]);
+        if (i < arrayToPrint.Length - 1)
+        {
+            Console.Write (", ");
+        }
+    }
+    Console.Write("] ");
+}
+
+System.Console.WriteLine("Enter array length, min n & max n for range: ");
+int arrayL = Convert.ToInt32(Console.ReadLine());
+int min = Convert.ToInt32(Console.ReadLine());
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] userArray = GetRandomArray(arrayL, min, max);
+PrintArray(userArray);
+System.Console.WriteLine();

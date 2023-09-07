5a3fe443a067dd03fa49b06d2ea9e1d455fe1366
SilentMary <playinglassbead@gmail.com>
2023-09-07 07:18:48 +0100

    finished

--- Task 38/Program.cs
+++ Task 38/Program.cs
@@ -2,17 +2,18 @@
 [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76 */
 
 Console.Clear();
-double[] GetRandomArray(int arrayLength1, int start, int end)
+
+double[] GetRandomArray(int arrayLength, double start, double end)
 {
-    int[] array = new int[arrayLength1];
+    double[] array = new double[arrayLength];
     for (int i = 0; i < array.Length; i++)
     {
-        array [i] = new Random().Next(start, end);
+        array [i] = Math.Round((new Random().NextDouble() * (end - start) + start), 2);
     }
     return array;
 }
 
-void PrintArray(int [] arrayToPrint)
+void PrintArray(double [] arrayToPrint)
 {
     Console.Write("[");
     for (int i = 0; i < arrayToPrint.Length; i++)
@@ -28,9 +29,32 @@ void PrintArray(int [] arrayToPrint)
 
 System.Console.WriteLine("Enter array length, min n & max n for range: ");
 int arrayL = Convert.ToInt32(Console.ReadLine());
-int min = Convert.ToInt32(Console.ReadLine());
-int max = Convert.ToInt32(Console.ReadLine());
+double min = Double.Parse(Console.ReadLine());
+double max = Double.Parse(Console.ReadLine());
 
-int[] userArray = GetRandomArray(arrayL, min, max);
+double GetMin(double[] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min) min = array[i];
+    }
+    return min;
+}
+
+double GetMax(double[] array)
+{
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max) max = array[i];
+    }
+    return max;
+}
+
+double[] userArray = GetRandomArray(arrayL, min, max);
+double minimal = GetMin(userArray);
+double maximal = GetMax(userArray);
 PrintArray(userArray);
 System.Console.WriteLine();
+System.Console.WriteLine($"The difference between max number {maximal} & min number {minimal} is {maximal - minimal}");

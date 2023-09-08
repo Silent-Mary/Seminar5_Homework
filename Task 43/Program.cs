acc318b23689bba82fc3f70e48bfe6574c3c52d9
SilentMary <playinglassbead@gmail.com>
2023-09-08 21:34:55 +0100

    Even copying task 43 killed me, I'm sure there's a simpler way to solve this, c'mon guys dis iz hell

--- Task 43/Program.cs
+++ Task 43/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями: 
+   y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+   b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+Console.Clear();
+
+const int COEFFICIENT = 0;
+const int CONSTANT = 1;
+const int X_COORD = 0;
+const int Y_COORD = 1;
+const int LINE1 = 1;
+const int LINE2 = 2;
+
+double[] lineData1 = InputLineData(LINE1);
+double[] lineData2 = InputLineData(LINE2);
+
+if (ValidateLines(lineData1, lineData2))
+{
+    double[] coord = FindCoords(lineData1, lineData2);
+    Console.Write($"Point where lines y = {lineData1[COEFFICIENT]} * x + {lineData1[CONSTANT]} and y = {lineData2[COEFFICIENT]} * x + {lineData2[CONSTANT]} cross");
+    Console.WriteLine($" has coordinates ({coord[X_COORD]}, {coord[Y_COORD]})");
+}
+
+double Prompt(string message)
+{
+    System.Console.Write(message); 
+    string value = Console.ReadLine();
+    double result = Convert.ToDouble(value);
+    return result;
+}
+
+double[] InputLineData(int numberOfLine)
+{
+    double[] lineData = new double[2];
+    lineData[COEFFICIENT] = Prompt($"Enter coefficient for line {numberOfLine}: ");
+    lineData[CONSTANT] = Prompt($"Enter constant for line {numberOfLine}: ");
+    return lineData;
+}
+
+double[] FindCoords(double[] lineData1, double[] lineData2)
+{
+    double[] coord = new double[2];
+    coord[X_COORD] = (lineData1[CONSTANT] - lineData2[CONSTANT]) / (lineData2[COEFFICIENT] - lineData1[COEFFICIENT]);
+    coord[Y_COORD] = lineData1[CONSTANT] * coord[X_COORD] + lineData1[CONSTANT];
+    return coord;
+}
+
+bool ValidateLines(double[] lineData1, double[] lineData2)
+{
+    if (lineData1[COEFFICIENT] == lineData2[COEFFICIENT])
+    {
+        if (lineData1[CONSTANT] == lineData2[CONSTANT])
+        {
+            Console.WriteLine("Lines are identical");
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("Lines are parallel");
+            return false;
+        }
+    }
+    return true;
+}
+

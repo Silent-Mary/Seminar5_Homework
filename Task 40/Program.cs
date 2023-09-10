4c4258d62da690e38ef6ebdbe736080e50b4c86b
SilentMary <playinglassbead@gmail.com>
2023-09-10 16:11:25 +0100

    task 40 done

--- Task 40/Program.cs
+++ Task 40/Program.cs
@@ -1,2 +1,32 @@
 ﻿/* Gets 3 numbers and checks if there may exist a triangle with such length sides */ 
+Console.Clear();
+Console.Write("Enter 3 lengths: ");
+// int a = Convert.ToInt32(Console.ReadLine());
 
+int[] FillSidesArray()
+{
+    int[] array = new int[3];
+    for (int i = 0; i < 3; i++)
+    {
+        array [i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+bool MaybeTriangle(int[] array)
+{
+    if (array[0] < array[1] + array[2]
+    && array[1] < array[0] + array[2]
+    && array[2] < array[0] + array[1])
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int[] userArray = FillSidesArray();
+bool result = MaybeTriangle(userArray);
+Console.WriteLine(result);

63682a76790f14b77819a0c20561fbffb950ec18
SilentMary <playinglassbead@gmail.com>
2023-09-10 16:28:12 +0100

    task 42 done also

--- Task 42/Program.cs
+++ Task 42/Program.cs
@@ -1,1 +1,20 @@
 ﻿/* Turns a denary number into binary one */ 
+Console.Clear();
+Console.WriteLine("Enter a denary number: ");
+int denary = Convert.ToInt32(Console.ReadLine());
+
+string Division(int denary)
+{
+    string result = string.Empty;
+    int temp = 0;
+    for (int i = denary; i > 0; i /= 2)
+    {
+        temp = i % 2;
+        result = temp + result;
+    }
+
+return result;
+}
+
+string NoName = Division(denary);
+Console.WriteLine(NoName);
